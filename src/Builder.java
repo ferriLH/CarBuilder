4dad0f4b7495f3c65a4f92c696861d6ed24000e0
Ferri Lasmi Halim <37891697+ferriLH@users.noreply.github.com>
2020-08-05 17:37:48 +0700

    Add files via upload

---
+++ src/Builder.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ferri L Halim
+ */
+
+
+
+public interface Builder {
+    void setType(Type type);
+    void setSeats(int seats);
+    void setEngine(Engine engine);
+    void setTransmission(Transmission transmission);
+    void setTripComputer(TripComputer tripComputer);
+    void setGPSNavigator(GPSNavigator gpsNavigator);
+}
